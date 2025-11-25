cdb1d036c77285307492af290243af482a4b4086
Andrey Fidrya <findhostcoupons@gmail.com>
2025-11-25 21:52:19 +0200

    Map's block on the home page

--- resources/views/components/layouts/porto.blade.php
+++ resources/views/components/layouts/porto.blade.php
@@ -331,11 +331,17 @@
 
 								<div class="micro-map box-shadow-custom my-4 clearfix">
 									<div class="micro-map-map">
-										<div id="googleMapsMicro" class="google-map m-0" style="height: 260px;"></div>
+										<iframe
+											width="100%"
+											height="350"
+											style="border:0;"
+											loading="lazy"
+											allowfullscreen
+											src="https://www.google.com/maps?q=49.633768,36.34814&hl=ru&z=14&output=embed">
+										</iframe>
 									</div>
 									<div class="micro-map-info">
-										<div class="micro-map-info-detail">
-											
+										<div class="micro-map-info-detail">											
 											<div class="address-line">
 												<div class="header-extra-info-icon">
 													<i class="icon-location-pin icons text-color-primary"></i>
@@ -647,72 +653,6 @@
 
 		<!-- Theme Initialization Files -->
 		<script src="{{ asset('js/theme.init.js') }}"></script>
-
-	<script src="{{ asset('https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY') }}"></script>
-		<script>
-
-			function initializeGoogleMaps() {
-				// Map Markers
-				var mapMarkers = [{
-					address: "New York, NY 10017",
-					icon: {
-						image: "img/pin.png",
-						iconsize: [26, 46],
-						iconanchor: [12, 46]
-					}
-				}];
-
-				// Map Initial Location
-				var initLatitude = 40.75198;
-				var initLongitude = -73.96978;
-
-				$('#googleMapsMacro').gMap({
-					controls: {
-						draggable: (($.browser.mobile) ? false : true),
-						panControl: true,
-						zoomControl: true,
-						mapTypeControl: true,
-						scaleControl: true,
-						streetViewControl: true,
-						overviewMapControl: true
-					},
-					scrollwheel: false,
-					markers: mapMarkers,
-					latitude: initLatitude,
-					longitude: initLongitude,
-					zoom: 16
-				});
-
-				$('#googleMapsMicro').gMap({
-					controls: {
-						draggable: true,
-						zoomControl: true,
-						scaleControl: true
-					},
-					scrollwheel: false,
-					markers: mapMarkers,
-					latitude: initLatitude,
-					longitude: initLongitude,
-					zoom: 13
-				});
-
-				var mapRef = $('#googleMapsMacro, #googleMapsMicro').data('gMap.reference');
-
-				// Styles from https://snazzymaps.com/
-				var styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
-
-				var styledMap = new google.maps.StyledMapType(styles, {
-					name: 'Styled Map'
-				});
-
-				mapRef.mapTypes.set('map_style', styledMap);
-				mapRef.setMapTypeId('map_style');
-			}
-
-			// Initialize Google Maps when element enter on browser view
-			theme.fn.intObs( '.google-map', 'initializeGoogleMaps()', {} );
-
-		</script>
-
+		
 	</body>
 </html>
