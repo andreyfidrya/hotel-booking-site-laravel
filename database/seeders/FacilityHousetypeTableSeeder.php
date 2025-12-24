a5381f646ee196dfc3c1ec96c5b90694ae33ac56
Andrey Fidrya <findhostcoupons@gmail.com>
2025-12-24 14:46:19 +0200

    making some changes to seeds

--- database/seeders/FacilityHousetypeTableSeeder.php
+++ database/seeders/FacilityHousetypeTableSeeder.php
@@ -12,47 +12,47 @@ class FacilityHousetypeTableSeeder extends Seeder
         DB::table('facility_housetype')->insert([
             [
                 'facility_id' => '11',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ], 
             [
                 'facility_id' => '10',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '9',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '8',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '7',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '6',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '5',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '4',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '3',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '2',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],
             [
                 'facility_id' => '1',
-                'housetype_id' => '1',                               
+                'housetype_id' => '2',                               
             ],           
         ]);
     }
