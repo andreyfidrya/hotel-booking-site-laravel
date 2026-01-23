84a60f6e57bb85dbacfe9cc413c8f1fbc81c92b5
Andrey Fidrya <findhostcoupons@gmail.com>
2026-01-23 21:15:08 +0200

    planning admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,42 @@
 <x-layouts.admin>
-Домики.
+
+    <h1 class="mb-4">Домики</h1>
+
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th style="width: 15%">Название</th>
+                <th style="width: 25%">Описание</th>
+                <th style="width: 10%">Тип</th>
+                <th style="width: 20%">Основное фото</th>
+                <th style="width: 20%">Галерея</th>
+                <th style="width: 10%">Действия</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+                <tr>
+                    <td>название домика</td>
+                    <td>описание домика</td>
+                    <td>тип домика</td>
+                    <td>
+                        <img src="" 
+                             width="150">
+                    </td>
+                    <td>
+                        <img src="" 
+                             width="150">
+                    </td>
+                    <td>
+                        <a href="" 
+                           class="btn btn-sm btn-primary">
+                            Редактировать
+                        </a>
+                    </td>
+                </tr>
+            
+        </tbody>
+    </table>
+
 </x-layouts.admin>
+
