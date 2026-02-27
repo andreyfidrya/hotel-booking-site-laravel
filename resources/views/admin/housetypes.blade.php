8121bdd19542636315b00bda21eec5a2a9d11f5d
Andrey Fidrya <findhostcoupons@gmail.com>
2026-02-27 21:07:54 +0200

    displaying housetypes in admin panel

--- resources/views/admin/housetypes.blade.php
+++ resources/views/admin/housetypes.blade.php
@@ -4,9 +4,46 @@
 
     <a href="" 
        class="btn btn-success mb-4">
-        Добавить тип домика
+        Добавить Тип Домика
     </a>
 
-    
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th style="width: 70%">Название</th>
+                <th style="width: 30%">Действия</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($housetypes as $housetype)
+                <tr>
+                    <td class="text-center align-middle">
+                        {{ $housetype->name }}
+                    </td>
+
+                    <td class="text-center align-middle">
+                        <a href="" 
+                           class="btn btn-sm btn-primary w-100 mb-2">
+                            Редактировать
+                        </a>
+
+                        <form action="" 
+                              method="POST"
+                              onsubmit="return confirm('Удалить это удобство?')">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" 
+                                    class="btn btn-sm btn-danger w-100">
+                                Удалить
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 </x-layouts.admin>
+
+
