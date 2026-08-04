76f60c204700d3c1547336d336139f3c70d6681a
Andrey Fidrya <findhostcoupons@gmail.com>
2026-08-04 20:37:34 +0300

    10 adults and 10 children

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -127,6 +127,12 @@
 													<option value="2">2</option>
 													<option value="3">3</option>
 													<option value="4">4</option>
+													<option value="4">5</option>
+													<option value="4">6</option>
+													<option value="4">7</option>
+													<option value="4">8</option>
+													<option value="4">9</option>
+													<option value="4">10</option>
 												</select>
 											</div>
 										</div>
@@ -141,6 +147,12 @@
 													<option value="2">2</option>
 													<option value="3">3</option>
 													<option value="4">4</option>
+													<option value="4">5</option>
+													<option value="4">6</option>
+													<option value="4">7</option>
+													<option value="4">8</option>
+													<option value="4">9</option>
+													<option value="4">10</option>
 												</select>
 											</div>
 										</div>
