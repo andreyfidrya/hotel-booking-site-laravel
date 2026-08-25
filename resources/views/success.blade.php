2e1efaf6b6b16c08531d21905a3f30e2bf9f14da
Andrey Fidrya <findhostcoupons@gmail.com>
2026-08-25 22:56:54 +0300

    updating success booking page

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,3 +1,15 @@
 <x-layouts.porto>
-Бронирование успешно оформлено!
+
+    <div class="container py-5 text-center">
+        <h1>Бронирование успешно оформлено!</h1>
+
+        <p class="mt-3">
+            Спасибо за бронирование.
+        </p>
+
+        <a href="{{ route('home.index') }}" class="btn btn-primary mt-3">
+            Вернуться на главную
+        </a>
+    </div>
+
 </x-layouts.porto>
