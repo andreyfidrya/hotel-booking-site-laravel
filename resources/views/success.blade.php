42384341b821e2318f609b054a23c0103ad52ec9
Andrey Fidrya <findhostcoupons@gmail.com>
2026-08-29 21:06:08 +0300

    updating success page

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -49,14 +49,81 @@
                 {{ $booking->email }}
             </p>
 
+            <hr>
+
+            @auth
+
+                <div class="alert alert-success">
+                    <h4>Бронирование сохранено в вашем аккаунте</h4>
+
+                    <p class="mb-0">
+                        Вы можете войти в личный кабинет и просмотреть свои бронирования.
+                    </p>
+                </div>
+
+                <div class="text-center mt-3">
+                    <a href="{{ route('bookings.index') }}" class="btn btn-primary">
+                        Мои бронирования
+                    </a>
+                </div>
+
+            @else
+
+                @if($user)
+
+                    <div class="alert alert-info">
+                        <h4>У вас уже есть аккаунт</h4>
+
+                        <p>
+                            Пользователь с email
+                            <strong>{{ $booking->email }}</strong>
+                            уже зарегистрирован.
+                        </p>
+
+                        <p class="mb-0">
+                            Войдите в свой аккаунт, чтобы просматривать свои бронирования.
+                        </p>
+                    </div>
+
+                    <div class="text-center mt-3">
+                        <a href="{{ route('login') }}" class="btn btn-primary">
+                            Войти в аккаунт
+                        </a>
+                    </div>
+
+                @else
+
+                    <div class="alert alert-info">
+                        <h4>Хотите создать аккаунт?</h4>
+
+                        <p>
+                            У вас ещё нет аккаунта.
+                        </p>
+
+                        <p class="mb-0">
+                            Зарегистрируйтесь, чтобы в дальнейшем удобно
+                            просматривать свои бронирования.
+                        </p>
+                    </div>
+
+                    <div class="text-center mt-3">
+                        <a href="{{ route('register') }}" class="btn btn-primary">
+                            Зарегистрироваться
+                        </a>
+                    </div>
+
+                @endif
+
+            @endauth
+
         </div>
 
         <div class="text-center mt-4">
-            <a href="{{ route('home.index') }}" class="btn btn-primary">
+            <a href="{{ route('home.index') }}" class="btn btn-secondary">
                 Вернуться на главную
             </a>
         </div>
 
-    </div>    
+    </div>
 
 </x-layouts.porto>
